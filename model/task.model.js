494e4e7a41ed804b63df99c16d189b4c1ef0d96a
kulsoomrasheed <rasheedamaan111@gmail.com>
2024-03-29 07:00:14 +0530

    backend completed

--- model/task.model.js
+++ model/task.model.js
@@ -4,14 +4,12 @@ const taskSchema = mongoose.Schema({
     title: {
         type: String,
         required: [true, 'Title is required'],
-        minlength: [3, 'Title must be at least 3 characters long'],
-        maxlength: [50, 'Title cannot exceed 100 characters']
+      
     },
     desc: {
         type: String,
         required: [true, 'Description is required'],
-        minlength: [10, 'Description must be at least 10 characters long'],
-        maxlength: [1000, 'Description cannot exceed 1000 characters']
+       
     },
     status: {
         type: Boolean,
